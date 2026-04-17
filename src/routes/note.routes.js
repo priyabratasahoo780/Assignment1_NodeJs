1a15d855bc118ee47596874bbc6be6d8154c464f
priyabratasahoo780 <priyabrata.sahoo.cg@gmail.com>
2026-04-17 20:59:04 +0530

    feat: add delete note endpoint (DELETE /api/notes/:id)

--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { createNote, createNotesBulk, getAllNotes, getNoteById, replaceNote, updateNote } from "../controllers/note.controller.js";
-
+import { deleteNote } from "../controllers/note.controller.js";
 const router = express.Router();
 
 router.get("/", getAllNotes);
@@ -9,4 +9,5 @@ router.post("/bulk", createNotesBulk);
 router.get("/:id", getNoteById);
 router.put("/:id", replaceNote);
 router.patch("/:id", updateNote);
+router.delete("/:id", deleteNote);
 export default router;
